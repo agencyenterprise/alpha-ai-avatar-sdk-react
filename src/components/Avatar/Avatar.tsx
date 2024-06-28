72adc689fdf17bc5b1a9f3184bcc4a09ef58c51b
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2024-06-28 10:38:54 -0300

    Rename avatar component props

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -2,12 +2,12 @@ import { useWebAvatar } from '../../hooks/useWebAvatar';
 
 export type AvatarProps = React.ComponentPropsWithoutRef<'video'>;
 
-export function Avatar(rest: AvatarProps) {
+export function Avatar(props: AvatarProps) {
   const { videoRef, audioRef } = useWebAvatar();
 
   return (
     <>
-      <video ref={videoRef} autoPlay playsInline muted {...rest} />
+      <video ref={videoRef} autoPlay playsInline muted {...props} />
       <audio ref={audioRef} style={{ display: 'none' }} autoPlay muted />
     </>
   );
