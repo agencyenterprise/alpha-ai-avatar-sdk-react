b957dd02681cc0b6d8013690fac27569dba6f2da
William Sanches <wrsanches@outlook.com>
2024-07-02 14:54:36 -0300

    Improve AvatarClient test

--- src/core/__tests__/AvatarClient.test.ts
+++ src/core/__tests__/AvatarClient.test.ts
@@ -1,109 +1,173 @@
 import { AvatarClient } from '../AvatarClient';
 import {
+  AvatarClientConfig,
   CreateRoomResponse,
   GetAvatarsResponse,
   GetSupportedVoicesResponse,
 } from '../types';
 
-jest.mock('../AvatarClient');
+global.fetch = jest.fn();
 
 describe('AvatarClient', () => {
-  let apiKey: string;
-  let baseUrl: string;
-  let client: jest.Mocked<AvatarClient>;
+  const config: AvatarClientConfig = {
+    apiKey: 'test-key',
+    baseUrl: 'http://localhost:5000',
+    avatarId: 1,
+  };
 
-  let mockConnect: jest.Mock;
-  let mockGetAvatars: jest.Mock;
-  let mockGetSupportedVoices: jest.Mock;
+  let avatarClient: AvatarClient;
 
   beforeEach(() => {
-    apiKey = 'test-key';
-    baseUrl = 'http://localhost:5000';
-
-    jest.clearAllMocks();
-
-    mockConnect = jest.fn();
-    mockGetAvatars = jest.fn();
-    mockGetSupportedVoices = jest.fn();
-
-    (AvatarClient as jest.MockedClass<typeof AvatarClient>).mockImplementation(
-      () =>
-        ({
-          connect: mockConnect,
-          getAvatars: mockGetAvatars,
-          getSupportedVoices: mockGetSupportedVoices,
-        }) as unknown as AvatarClient,
-    );
-
-    client = new AvatarClient({
-      apiKey,
-    }) as jest.Mocked<AvatarClient>;
-  });
-
-  describe('constructor', () => {
-    it('should use the provided baseUrl and apiKey', () => {
-      new AvatarClient({
-        baseUrl,
-        apiKey,
-      });
-      expect(AvatarClient).toHaveBeenCalledWith({
-        baseUrl,
-        apiKey,
-      });
-    });
-
-    it('should use the default baseUrl if not provided', () => {
-      new AvatarClient({ apiKey });
-      expect(AvatarClient).toHaveBeenCalledWith({
-        apiKey,
-      });
-    });
+    avatarClient = new AvatarClient(config);
+    (fetch as jest.Mock).mockClear();
   });
 
   describe('connect', () => {
-    it('should call connect with the correct avatarId', async () => {
-      await client.connect(456);
-      expect(mockConnect).toHaveBeenCalledWith(456);
+    let mockResponse: CreateRoomResponse;
+
+    beforeEach(() => {
+      mockResponse = {
+        token: 'test-token',
+        serverUrl: 'https://livekit.io',
+      };
     });
 
-    it('should return the response from connect', async () => {
-      const mockResponse: CreateRoomResponse = {
-        serverUrl: 'http://livekit.com',
-        token: 'test-token',
-      };
-      mockConnect.mockResolvedValue(mockResponse);
-      const response = await client.connect(456);
+    it('should create a room with the provided avatarId', async () => {
+      (fetch as jest.Mock).mockResolvedValueOnce({
+        ok: true,
+        json: async () => mockResponse,
+      });
+
+      const response = await avatarClient.connect(456);
+
       expect(response).toEqual(mockResponse);
+      expect(fetch).toHaveBeenCalledWith(
+        'http://localhost:5000/rooms',
+        expect.objectContaining({
+          method: 'POST',
+          headers: expect.objectContaining({
+            'Content-Type': 'application/json',
+            'X-API-Key': 'test-key',
+          }),
+          body: JSON.stringify({ avatarId: 456 }),
+        }),
+      );
+    });
+
+    it('should create a room with the default avatarId from the client if none is provided', async () => {
+      (fetch as jest.Mock).mockResolvedValueOnce({
+        ok: true,
+        json: async () => mockResponse,
+      });
+
+      const response = await avatarClient.connect();
+
+      expect(response).toEqual(mockResponse);
+      expect(fetch).toHaveBeenCalledWith(
+        'http://localhost:5000/rooms',
+        expect.objectContaining({
+          method: 'POST',
+          headers: expect.objectContaining({
+            'Content-Type': 'application/json',
+            'X-API-Key': 'test-key',
+          }),
+          body: JSON.stringify({ avatarId: 1 }),
+        }),
+      );
+    });
+
+    it('should create a room with the avatarId as null if none is provided', async () => {
+      avatarClient = new AvatarClient({
+        apiKey: 'test-key',
+        baseUrl: 'http://localhost:5000',
+      });
+
+      (fetch as jest.Mock).mockResolvedValueOnce({
+        ok: true,
+        json: async () => mockResponse,
+      });
+
+      const response = await avatarClient.connect();
+
+      expect(response).toEqual(mockResponse);
+      expect(fetch).toHaveBeenCalledWith(
+        'http://localhost:5000/rooms',
+        expect.objectContaining({
+          method: 'POST',
+          headers: expect.objectContaining({
+            'Content-Type': 'application/json',
+            'X-API-Key': 'test-key',
+          }),
+          body: '{}',
+        }),
+      );
+    });
+
+    it('should create a room with the production base url if none is provided', async () => {
+      avatarClient = new AvatarClient({
+        apiKey: 'test-key',
+      });
+
+      (fetch as jest.Mock).mockResolvedValueOnce({
+        ok: true,
+        json: async () => mockResponse,
+      });
+
+      const response = await avatarClient.connect();
+
+      expect(response).toEqual(mockResponse);
+      expect(fetch).toHaveBeenCalledWith(
+        'https://avatar.alpha.school/rooms',
+        expect.objectContaining({
+          method: 'POST',
+          headers: expect.objectContaining({
+            'Content-Type': 'application/json',
+            'X-API-Key': 'test-key',
+          }),
+          body: '{}',
+        }),
+      );
     });
   });
 
   describe('getAvatars', () => {
-    it('should call getAvatars', async () => {
-      await client.getAvatars();
-      expect(mockGetAvatars).toHaveBeenCalled();
-    });
-
-    it('should return the response from getAvatars', async () => {
+    it('should fetch the list of avatars', async () => {
       const mockResponse: GetAvatarsResponse = [
         {
           id: 1,
           name: 'Avatar 1',
-          thumbnail: 'http://localhost:5000/avatar-1.png',
+          thumbnail: 'http://localhost:5000/avatar1.png',
+        },
+        {
+          id: 2,
+          name: 'Avatar 2',
+          thumbnail: 'http://localhost:5000/avatar2.png',
         },
       ];
-      mockGetAvatars.mockResolvedValue(mockResponse);
-      const response = await client.getAvatars();
+
+      (fetch as jest.Mock).mockResolvedValueOnce({
+        ok: true,
+        json: async () => mockResponse,
+      });
+
+      const response = await avatarClient.getAvatars();
+
       expect(response).toEqual(mockResponse);
+      expect(fetch).toHaveBeenCalledWith(
+        'http://localhost:5000/avatars',
+        expect.objectContaining({
+          method: 'GET',
+          headers: expect.objectContaining({
+            'Content-Type': 'application/json',
+            'X-API-Key': 'test-key',
+          }),
+        }),
+      );
     });
   });
 
   describe('getSupportedVoices', () => {
-    it('should call getSupportedVoices', async () => {
-      await client.getSupportedVoices();
-      expect(mockGetSupportedVoices).toHaveBeenCalled();
-    });
-
-    it('should return the response from getSupportedVoices', async () => {
+    it('should fetch the list of supported voices', async () => {
       const mockResponse: GetSupportedVoicesResponse = [
         {
           displayName: 'Guy',
@@ -126,9 +190,25 @@ describe('AvatarClient', () => {
           wordsPerMinute: '215',
         },
       ];
-      mockGetSupportedVoices.mockResolvedValue(mockResponse);
-      const response = await client.getSupportedVoices();
+
+      (fetch as jest.Mock).mockResolvedValueOnce({
+        ok: true,
+        json: async () => mockResponse,
+      });
+
+      const response = await avatarClient.getSupportedVoices();
+
       expect(response).toEqual(mockResponse);
+      expect(fetch).toHaveBeenCalledWith(
+        'http://localhost:5000/supported-voices',
+        expect.objectContaining({
+          method: 'GET',
+          headers: expect.objectContaining({
+            'Content-Type': 'application/json',
+            'X-API-Key': 'test-key',
+          }),
+        }),
+      );
     });
   });
 });
