812981b1b1bf5430d4bb39e2173a29ef8c7d258f
Victor Dias <victordias07@gmail.com>
2024-10-17 22:04:06 -0300

    fix: ts error on speak when overrideVoice is not defined

--- src/core/ai-pi/ManualAvatarController.ts
+++ src/core/ai-pi/ManualAvatarController.ts
@@ -2,7 +2,7 @@ import { SynthesizerOptions } from 'alpha-ai-avatar-sdk-js';
 import { BaseAvatarController } from './BaseAvatarController';
 
 export class ManualAvatarController extends BaseAvatarController {
-  speak(text: string, overrideVoice: SynthesizerOptions) {
+  speak(text: string, overrideVoice?: SynthesizerOptions) {
     this.avatarClient.say(text, overrideVoice);
   }
 }
